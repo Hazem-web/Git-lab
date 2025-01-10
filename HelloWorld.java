d0b2b93e77ef18e6ec321e364d3283dff3026778
KhairySuleiman4 <khairy.hatem4@gmail.com>
2025-01-10 16:19:40 +0200

    sum_method edited

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloJava{
     public void printHelloWorld(){
         System.out.println("Hello World!");
-        this.sum_method(10, 5);
+        this.sum_method(10, 1);
     }
 
     public static int sum_method(int a, int b){
