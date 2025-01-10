cf042db5eb53b3d3961b46e915c0e8d33ddf5842
KhairySuleiman4 <khairy.hatem4@gmail.com>
2025-01-10 17:09:26 +0200

    sum_method name changed to sumTwoNumbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 class HelloJava{
     public void printHelloWorld(){
         System.out.println("Hello World!");
-        this.sum_method(10, 1);
+        this.sumTwoNumbers(10, 1);
     }
 
-    public static int sum_method(int a, int b){
+    public static int sumTwoNumbers(int a, int b){
         return a + b;
     }
 	public void printMyName(){
