58ad1d2dffcf4c0ebcf691a32e3ba4a097131a87
KhairySuleiman4 <khairy.hatem4@gmail.com>
2025-01-10 16:03:12 +0200

    sum_method created and called from the main method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloJava{
     public void printHelloWorld(){
         System.out.println("Hello World!");
+        this.sum_method(10, 5);
+    }
+
+    public static int sum_method(int a, int b){
+        return a + b;
     }
 }
